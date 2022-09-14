c6fbdcb8bb135dad9f3256f4a817184bb8b39334
Misael <misaelcudek@gmail.com>
2022-09-14 16:25:38 -0300

    Add files via upload

---
+++ Recursividad/decAbin_recursividad.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+#include<math.h>
+
+int deciAbin(int n);
+
+int main ()
+{
+     printf("%d\n",deciAbin(255)); 
+
+}
+
+int deciAbin(int n)
+{   static int cifras=0;
+    
+    
+    int result=0;
+    if(n==1)
+    {
+        cifras++;
+        printf("cifras %d\n ",cifras);
+        return(n*pow(10,cifras-1));
+    }else
+    {   
+        result=n%2;
+        n=n/2;
+        printf("Adelante %d \n",result);
+        cifras++;
+        return(result*pow(10,cifras-1) + deciAbin(n) );
+    }
+}
