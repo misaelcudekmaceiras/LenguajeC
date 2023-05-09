c5c6b390d30eb2d34bd288037148f0e2c2b67f8b
Misael <misaelcudek@gmail.com>
2023-05-09 07:34:06 -0300

    Add files via upload

---
+++ variables/desbordenosignado.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+
+
+int main()
+{
+unsigned  int  a=(4294967295);//2³²-1
+unsigned  int b=5;
+unsigned  int r=0;
+r=a+b;
+printf("El resultado es %d\n",r);
+
+
+return(0);
+}
---
+++ variables/desbordesignado.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+
+
+int main()
+{
+ int  a=(2147483647);//2³²
+ int b=0;
+ int r=0;
+r=a+b;
+printf("El resultado es %d\n",r);
+
+
+return(0);
+}
