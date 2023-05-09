6a9743490d779aa27aeed5f1a706c7d9fadd0892
Misael <misaelcudek@gmail.com>
2023-05-09 07:35:05 -0300

    Add files via upload

---
+++ variables/float.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+int main()
+{
+    double num=2.7;
+    float num2=2.7;
+   // printf("%.30f\n",num);
+   // printf("%.30f\n",num2);
+
+     printf("%f\n",num);
+     printf("%f\n",num2);
+
+return(0);
+}
