dff1f26335e782766a83912926e928a5f0133b0e
Misael <misaelcudek@gmail.com>
2023-05-31 22:04:40 -0300

    Add files via upload

---
+++ punteros/variables.s
@@ -0,0 +1,53 @@
+	.file	"variables.c"
+# GNU C17 (Ubuntu 11.3.0-1ubuntu1~22.04.1) version 11.3.0 (x86_64-linux-gnu)
+#	compiled by GNU C version 11.3.0, GMP version 6.2.1, MPFR version 4.1.0, MPC version 1.2.1, isl version isl-0.24-GMP
+
+# GGC heuristics: --param ggc-min-expand=100 --param ggc-min-heapsize=131072
+# options passed: -mtune=generic -march=x86-64 -fasynchronous-unwind-tables -fstack-protector-strong -fstack-clash-protection -fcf-protection
+	.text
+	.globl	main
+	.type	main, @function
+main:
+.LFB0:
+	.cfi_startproc
+	endbr64	
+	pushq	%rbp	#
+	.cfi_def_cfa_offset 16
+	.cfi_offset 6, -16
+	movq	%rsp, %rbp	#,
+	.cfi_def_cfa_register 6
+# variables.c:6:     int a=6;
+	movl	$6, -4(%rbp)	#, a
+# variables.c:7:     short b=2;
+	movw	$2, -6(%rbp)	#, b
+# variables.c:8:     char c=3;
+	movb	$3, -8(%rbp)	#, c
+# variables.c:9:     unsigned char d=7;
+	movb	$7, -7(%rbp)	#, d
+# variables.c:16: return(0);
+	movl	$0, %eax	#, _5
+# variables.c:17: }
+	popq	%rbp	#
+	.cfi_def_cfa 7, 8
+	ret	
+	.cfi_endproc
+.LFE0:
+	.size	main, .-main
+	.ident	"GCC: (Ubuntu 11.3.0-1ubuntu1~22.04.1) 11.3.0"
+	.section	.note.GNU-stack,"",@progbits
+	.section	.note.gnu.property,"a"
+	.align 8
+	.long	1f - 0f
+	.long	4f - 1f
+	.long	5
+0:
+	.string	"GNU"
+1:
+	.align 8
+	.long	0xc0000002
+	.long	3f - 2f
+2:
+	.long	0x3
+3:
+	.align 8
+4:
