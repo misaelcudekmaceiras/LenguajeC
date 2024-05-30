0fe5e03582d66c316b5389a3fc70b5f6c2765ae4
Misael <misaelcudek@gmail.com>
2024-05-30 08:02:50 -0300

    Add files via upload

---
+++ String/char_a_num.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include <stdlib.h>
+
+
+int main()
+
+{
+    int numA=0;
+    float numB=0;
+    numA=atoi("123");
+    numB=atof("12.45");
+    printf("El numero entero es %d\n",numA);
+    printf("El numero float/double es %f\n",numB);
+
+
+return(0);
+}
